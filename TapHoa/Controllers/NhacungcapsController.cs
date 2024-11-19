aee7c183d4db6c91c1e47b5c85e24f9560638271
Loc24102004 <phuonghuugialoc@siu.edu.vn>
2024-11-19 20:53:26 +0800

    feat: Add function shoppage

---
+++ TapHoa/Controllers/NhacungcapsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TapHoa.Data;
+
+namespace TapHoa.Controllers
+{
+    public class NhacungcapsController : Controller
+    {
+        private readonly TaphoaContext _context;
+
+        public NhacungcapsController(TaphoaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Nhacungcaps
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Nhacungcaps.ToListAsync());
+        }
+
+        // GET: Nhacungcaps/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhacungcap = await _context.Nhacungcaps
+                .FirstOrDefaultAsync(m => m.Mancc == id);
+            if (nhacungcap == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhacungcap);
+        }
+
+        // GET: Nhacungcaps/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Nhacungcaps/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Mancc,Tenncc")] Nhacungcap nhacungcap)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(nhacungcap);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nhacungcap);
+        }
+
+        // GET: Nhacungcaps/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhacungcap = await _context.Nhacungcaps.FindAsync(id);
+            if (nhacungcap == null)
+            {
+                return NotFound();
+            }
+            return View(nhacungcap);
+        }
+
+        // POST: Nhacungcaps/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Mancc,Tenncc")] Nhacungcap nhacungcap)
+        {
+            if (id != nhacungcap.Mancc)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nhacungcap);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NhacungcapExists(nhacungcap.Mancc))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nhacungcap);
+        }
+
+        // GET: Nhacungcaps/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhacungcap = await _context.Nhacungcaps
+                .FirstOrDefaultAsync(m => m.Mancc == id);
+            if (nhacungcap == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhacungcap);
+        }
+
+        // POST: Nhacungcaps/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var nhacungcap = await _context.Nhacungcaps.FindAsync(id);
+            if (nhacungcap != null)
+            {
+                _context.Nhacungcaps.Remove(nhacungcap);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NhacungcapExists(int id)
+        {
+            return _context.Nhacungcaps.Any(e => e.Mancc == id);
+        }
+    }
+}
