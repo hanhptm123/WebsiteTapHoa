acf9c325babdd80000f02cead1fa7e2c7aff5888
hanhptm123 <studyhanh@gmail.com>
2024-11-20 08:23:46 +0700

    feat(user): View product for user

---
+++ TapHoa/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TapHoa.Data;
+
+namespace TapHoa.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly TaphoaContext _context;
+
+        public ProductController(TaphoaContext context)
+        {
+            _context = context;
+        }
+
+        private bool SanphamExists(int id)
+        {
+            return _context.Sanphams.Any(e => e.Masp == id);
+        }
+        public async Task<IActionResult> ViewProduct()
+        {
+            // Fetch the list of products with related data
+            var taphoaContext = await _context.Sanphams
+                .Include(s => s.MakmNavigation)      // Include related Makm
+                .Include(s => s.MaloaispNavigation)   // Include related Maloaisp
+                .Include(s => s.ManccNavigation)      // Include related Mancc
+                .Include(s => s.MathNavigation)       // Include related Math
+                .ToListAsync(); // Asynchronously get the list of Sanpham
+
+            // Return the view with the product list
+            return View(taphoaContext);
+        }
+        public async Task<IActionResult> ProductByCategory(int? id)
+        {
+            // Lọc sản phẩm theo loại
+            var products = await _context.Sanphams
+                .Include(s => s.MakmNavigation) // Include các bảng liên quan
+                .Include(s => s.MaloaispNavigation)
+                .Include(s => s.ManccNavigation)
+                .Include(s => s.MathNavigation)
+                .Where(p => p.Maloaisp == id) // Lọc theo Maloaisp
+                .ToListAsync();
+
+            // Trả về view danh sách sản phẩm
+            return View(products);
+        }
+    }
+}
