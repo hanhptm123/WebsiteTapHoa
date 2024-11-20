94d6a0319e0517764a9c5aaa818e4cc1d199b492
Vo Duy Khang <voduykhang@Vos-MacBook-Pro.local>
2024-11-20 09:23:22 +0700

    feat(Address_function): add CRUD functionality for user

--- TapHoa/Controllers/AddressController.cs
+++ TapHoa/Controllers/AddressController.cs
@@ -1,140 +1,94 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TapHoa.Data;
+using X.PagedList;
+using X.PagedList.Extensions;
 
-namespace TapHoa.Controllers
+namespace TapHoa.Areas.Admin.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize] // Only allow authenticated users
-    public class AddressController : ControllerBase
+    [Route("address")]
+    public class AddressController : Controller
     {
-        private readonly TapHoaContext _context;
+        private readonly TaphoaContext _context;
 
-        public AddressController(TapHoaContext context)
+        public AddressController(TaphoaContext context)
         {
             _context = context;
         }
 
-        // Get list of Provinces/Regions
-        [HttpGet("GetProvinces")]
-        public IActionResult GetProvinces()
+        [Route("AddressList")]
+        public IActionResult AddressList(int? page)
         {
-            var provinces = _context.Provinces
-                .Select(p => new { p.Id, p.Name })
-                .ToList();
-            return Ok(provinces);
+            int pageSize = 8;
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+            var addresses = _context.Diachis.AsNoTracking().OrderBy(x => x.Madc).ToPagedList(pageNumber, pageSize);
+            return View(addresses);
         }
 
-        // Get list of Districts by Province/Region
-        [HttpGet("GetDistricts/{provinceId}")]
-        public IActionResult GetDistricts(int provinceId)
+        [Route("Create")]
+        [HttpGet]
+        public IActionResult Create()
         {
-            var districts = _context.Districts
-                .Where(d => d.ProvinceId == provinceId)
-                .Select(d => new { d.Id, d.Name })
-                .ToList();
-            return Ok(districts);
+            return View();
         }
 
-        // Get list of Wards by District
-        [HttpGet("GetWards/{districtId}")]
-        public IActionResult GetWards(int districtId)
+        [Route("Create")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Diachi address)
         {
-            var wards = _context.Wards
-                .Where(w => w.DistrictId == districtId)
-                .Select(w => new { w.Id, w.Name })
-                .ToList();
-            return Ok(wards);
+            if (ModelState.IsValid)
+            {
+                _context.Diachis.Add(address);
+                _context.SaveChanges();
+                TempData["Message"] = "Address created successfully";
+                return RedirectToAction("AddressList");
+            }
+            return View(address);
         }
 
-        // Get list of customer addresses
-        [HttpGet("GetAddresses")]
-        public IActionResult GetAddresses()
+        [Route("Edit")]
+        [HttpGet]
+        public IActionResult Edit(int Madc)
         {
-            // Get current user information
-            var customerId = GetCurrentUserId();
-
-            var addresses = _context.Diachis
-                .Where(a => a.Makh == customerId)
-                .Select(a => new
-                {
-                    a.Madc,
-                    a.Tennguoinhan,
-                    a.Sdt,
-                    a.Diachi1
-                })
-                .ToList();
-
-            return Ok(addresses);
-        }
-
-        // Add a new address
-        [HttpPost("AddAddress")]
-        public IActionResult AddAddress([FromBody] Diachi address)
-        {
+            var address = _context.Diachis.Find(Madc);
             if (address == null)
             {
-                return BadRequest("Invalid address information.");
+                return NotFound();
             }
-
-            var customerId = GetCurrentUserId();
-            address.Makh = customerId;
-
-            _context.Diachis.Add(address);
-            _context.SaveChanges();
-
-            return Ok("Address has been successfully added.");
+            return View(address);
         }
 
-        // Update an existing address
-        [HttpPut("UpdateAddress/{id}")]
-        public IActionResult UpdateAddress(int id, [FromBody] Diachi updatedAddress)
+        [Route("Edit")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Diachi address)
         {
-            var existingAddress = _context.Diachis.FirstOrDefault(a => a.Madc == id && a.Makh == GetCurrentUserId());
-
-            if (existingAddress == null)
+            if (ModelState.IsValid)
             {
-                return NotFound("Address does not exist or you do not have permission to edit it.");
+                _context.Update(address);
+                _context.SaveChanges();
+                TempData["Message"] = "Address updated successfully";
+                return RedirectToAction("AddressList");
             }
-
-            existingAddress.Tennguoinhan = updatedAddress.Tennguoinhan;
-            existingAddress.Sdt = updatedAddress.Sdt;
-            existingAddress.Diachi1 = updatedAddress.Diachi1;
-
-            _context.Entry(existingAddress).State = EntityState.Modified;
-            _context.SaveChanges();
-
-            return Ok("Address has been updated.");
+            return View(address);
         }
 
-        // Delete an address
-        [HttpDelete("DeleteAddress/{id}")]
-        public IActionResult DeleteAddress(int id)
+        [Route("Delete")]
+        [HttpGet]
+        public IActionResult Delete(int Madc)
         {
-            var address = _context.Diachis.FirstOrDefault(a => a.Madc == id && a.Makh == GetCurrentUserId());
+            var address = _context.Diachis.Find(Madc);
 
             if (address == null)
             {
-                return NotFound("Address does not exist or you do not have permission to delete it.");
+                return NotFound();
             }
 
             _context.Diachis.Remove(address);
             _context.SaveChanges();
-
-            return Ok("Address has been deleted.");
-        }
-
-        // Method to get the current user ID
-        private int GetCurrentUserId()
-        {
-            if (User.Identity?.IsAuthenticated ?? false)
-            {
-                // Assume User.Identity.Name contains the CustomerId
-                return int.Parse(User.Identity.Name ?? "0");
-            }
-            throw new UnauthorizedAccessException("User is not logged in.");
+            TempData["Message"] = "Address deleted successfully";
+            return RedirectToAction("AddressList");
         }
     }
 }
