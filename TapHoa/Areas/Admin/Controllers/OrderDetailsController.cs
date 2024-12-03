ff01b720edd79f78b15d9a0d59fea7fac78127ce
hanhptm123 <studyhanh@gmail.com>
2024-12-03 22:42:22 +0700

    Revert "refactor: all feature of MyHanh"

--- TapHoa/Areas/Admin/Controllers/OrderDetailsController.cs
+++ TapHoa/Areas/Admin/Controllers/OrderDetailsController.cs
@@ -21,6 +21,10 @@ namespace TapHoa.Areas.Admin.Controllers
             _context = context;
         }
 
+
+
+
+        //Feature:View order details for admin
         [Route("ViewOrderDetails")]
         public async Task<IActionResult> ViewOrderDetails(int? id)
         {
